69b22b7857cac3bf6112ea3e44bce66db3f5b913
daniele d'angeli <dangeli88.daniele@gmail.com>
2013-12-26 20:15:48 +0100

    init

---
+++ content/themes/custom_casper/assets/js/index.js
@@ -0,0 +1,33 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        $('.ui.post.fadedown')
+            .transition({
+                animation : 'fade down',
+                duration  : '1s'
+            })
+        ;
+
+        $('.ui.post.fadeup')
+            .transition({
+                animation : 'fade down',
+                duration  : '1s'
+            })
+        ;
+
+        // On the home page, move the blog icon inside the header 
+        // for better relative/absolute positioning.
+
+        //$("#blog-logo").prependTo("#site-head-content");
+
+    });
+
+}(jQuery));
