cd8547eb71ac561958e05d3ce0517de75d33cd8d
daniele d'angeli <dangeli88.daniele@gmail.com>
2013-12-30 20:45:34 +0100

    analytics

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,17 +6,26 @@ set :deploy_to, '/home/blog.danieledangeli'
 set :scm,         :git
 set :branch, "master"
 
+
 namespace :deploy do
 
-  desc "Stop Forever"
-   task :stop do
-     run "sudo forever stopall"
+   desc "Start forever"
+   task :start_forever do
+     on roles(:app) do
+       execute "cd #{current_path} && sudo forever start index.js"
+     end
    end
 
-   desc "Start Forever"
-     task :start do
-        run "cd #{current_path} && sudo forever start index.js"
+   desc "Symlink data"
+   task :symlink do
+     on roles(:app), in: :sequence, wait: 2 do
+       execute "ln -s #{shared_path}/data #{release_path}/content/data"
+       execute "ln -s #{shared_path}/images #{release_path}/content/images"
+       execute "ln -s #{shared_path}/plugins #{release_path}/content/plugins"
+       execute "cd #{current_path} && forever start index.js"
      end
+   end
+
 
   desc 'Restart application'
   task :restart do
@@ -25,15 +34,9 @@ namespace :deploy do
     end
   end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
 
+  after :deploy, 'deploy:symlink', 'start_forever'
   after :finishing, 'deploy:cleanup'
 
 end
+
